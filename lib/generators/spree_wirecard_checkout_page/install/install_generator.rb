4dfa774127411d998c7f28877f9b430046e78f7d
Long Nguyen <longn@vinagility.vn>
2015-06-08 23:29:19 +0700

    Upgrade to spree 2.3.4

--- lib/generators/spree_wirecard_checkout_page/install/install_generator.rb
+++ lib/generators/spree_wirecard_checkout_page/install/install_generator.rb
@@ -4,16 +4,6 @@ module SpreeWirecardCheckoutPage
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_wirecard_checkout_page\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_wirecard_checkout_page\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_wirecard_checkout_page\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_wirecard_checkout_page\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_wirecard_checkout_page'
       end
